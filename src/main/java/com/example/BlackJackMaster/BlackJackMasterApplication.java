14c5ab06efd09e855907a5bacff8ed79e433c463
IngaD89 <inga.demetrashvili.1@gmail.com>
2025-02-19 14:57:33 +0100

    Initial commit

---
+++ src/main/java/com/example/BlackJackMaster/BlackJackMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.BlackJackMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackJackMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackJackMasterApplication.class, args);
+	}
+
+}
